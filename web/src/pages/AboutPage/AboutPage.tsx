168275cd3099c2a21478c09e82b5b4164f0bf10c
AlejandroAS01 <Alejandroaguilarsabuco@hotmail.com>
2023-11-07 11:08:20 +0100

    first commit

---
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,27 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+
+      <header>
+        <h1>Redwood Blog</h1>
+        <nav>
+          <ul>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <p>
+          This site was created to demonstrate my mastery of Redwood: Look on my
+          works, ye mighty, and despair!
+        </p>
+
+      </main>
+    </>
+  )
+}
+
+export default AboutPage
